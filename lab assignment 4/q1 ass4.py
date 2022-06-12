7985fd3634cfecb4fd2f02286d67b91f72b5296f
HARSHIT-TYAGI-BT <104314533+HARSHIT-TYAGI-BT@users.noreply.github.com>
2022-06-12 17:05:49 +0530

    Update q1 ass4.py

--- lab assignment 4/q1 ass4.py
+++ lab assignment 4/q1 ass4.py
@@ -1,10 +1,9 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 #question 1
 #input marks:
 
 Marks = float(input('Enter your Marks: '))
-Enter your Marks: 75
+
 if Marks<25 :
     print('Your Grade is F')
 elif 25<=Marks<45 :
