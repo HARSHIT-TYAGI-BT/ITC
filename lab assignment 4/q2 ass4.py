8e0d610c4fbd95ecbfbebedd3744bf3189d5d885
HARSHIT-TYAGI-BT <104314533+HARSHIT-TYAGI-BT@users.noreply.github.com>
2022-06-12 17:06:53 +0530

    Update q2 ass4.py

--- lab assignment 4/q2 ass4.py
+++ lab assignment 4/q2 ass4.py
@@ -1,9 +1,8 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 #QUESTON 2
 # Input Year:
 year = int(input('Enter Year: '))
-Enter Year: 2300
+
 if year % 100 == 0:
      if year % 400 ==0:
           print('Leap Year')
