a2b0376be90842330a7a4b14550a0fd81e0e44d3
HARSHIT-TYAGI-BT <104314533+HARSHIT-TYAGI-BT@users.noreply.github.com>
2022-07-03 16:01:22 +0530

    Update assignment 6 output.py

--- ASSIGNMENT 6/assignment 6 output.py
+++ ASSIGNMENT 6/assignment 6 output.py
@@ -1,13 +1,12 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-======== RESTART: C:\Users\HP\Desktop\PYTHON ASSIGNMENTS\ASSIGNMENT 6.py =======
+#question1
 Enter a number: 6
 6  is a perfect number
 
+#question 2
 Enter a String: NUN
 NUN is a palindrome
 
+#question 3
 Enter a number: 5
       1 
      1 1 
@@ -15,12 +14,16 @@ Enter a number: 5
    1 3 3 1 
   1 4 6 4 1 
 
+#question 4 
 Enter a String: the quick brown fox jumps over the lazy dog
 Yes 'the quick brown fox jumps over the lazy dog' is a panagram
 
+#question 5
 Enter a hyphen separated sentence: mango-banana-apple-grapes
 apple-banana-grapes-mango
 
+
+#question 6
 Student ID= 21102040
 Student ID= 21102040
 Student Name= Harshit Tyagi
@@ -28,6 +31,8 @@ Student ID= 21102040
 Student Name= Harshit Tyagi
 Student Class= 1st Year
 
+
+#question 7
 True
 False
 True
@@ -37,6 +42,8 @@ Check whether the said classes are subclasses of the built-in object class or no
 True
 True
 
+
+#question8
 Enter number of elements : 6
 -4
 3
@@ -47,5 +54,7 @@ Enter number of elements : 6
 -4 3 1
 5 6 -11
 None
+
+#question9
 Enter the sequence of parantheses : {}[]
 {}[] - is balanced
