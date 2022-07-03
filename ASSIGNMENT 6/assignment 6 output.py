61902a1fb90e641fbab8fd81f159b3ea251667b9
HARSHIT-TYAGI-BT <104314533+HARSHIT-TYAGI-BT@users.noreply.github.com>
2022-07-03 15:58:43 +0530

    Add files via upload

---
+++ ASSIGNMENT 6/assignment 6 output.py
@@ -0,0 +1,51 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\HP\Desktop\PYTHON ASSIGNMENTS\ASSIGNMENT 6.py =======
+Enter a number: 6
+6  is a perfect number
+
+Enter a String: NUN
+NUN is a palindrome
+
+Enter a number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+Enter a String: the quick brown fox jumps over the lazy dog
+Yes 'the quick brown fox jumps over the lazy dog' is a panagram
+
+Enter a hyphen separated sentence: mango-banana-apple-grapes
+apple-banana-grapes-mango
+
+Student ID= 21102040
+Student ID= 21102040
+Student Name= Harshit Tyagi
+Student ID= 21102040
+Student Name= Harshit Tyagi
+Student Class= 1st Year
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter number of elements : 6
+-4
+3
+1
+5
+6
+-11
+-4 3 1
+5 6 -11
+None
+Enter the sequence of parantheses : {}[]
+{}[] - is balanced
